a46545a202a1c949a7a4fdf859e3e1fa16763c38
Ikpia <echukwuka97@gmail.com>
2023-10-24 08:58:04 -0700

    added new functionality

--- controllers/adminController.ts
+++ controllers/adminController.ts
@@ -2,9 +2,18 @@ import {Request, Response, NextFunction} from "express"
 import {createVendorInputs} from "../dto/vendor.dto"
 import {Vendor} from "../models"
 
+export const findVendor = async (id: string | undefined, email?: string) => {
+    if (id != undefined) {
+        return await Vendor.findById(id)
+    } else if (email) {
+        return await Vendor.findOne({email: email})
+    } else {
+        return null
+    }
+}
 export const createVendor = async (req: Request, res: Response, next: NextFunction) => {
     const {name, owenerName, foodType, pincode, address, phone, password, email} = <createVendorInputs>req.body
-    const registeredVendor = await Vendor.findOne({email: email})
+    const registeredVendor = await findVendor(email)
     const vendor = new Vendor({ ...req.body, serviceAvailable: true, coverImage: [], rating: 3})
     if (!registeredVendor) {
         vendor.save().then(() => {
@@ -18,9 +27,18 @@ export const createVendor = async (req: Request, res: Response, next: NextFuncti
 }
 
 export const getAllVendors = async (req: Request, res: Response, next: NextFunction) => {
-    
+    const vendors = await Vendor.find({})
+    res.json(vendors)
 }
 
-export const getVendor = async (req: Request, res: Response, next: NextFunction) => {
-    
+export const getVendorById = async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params
+    try {
+        const vendor = await findVendor(id)
+        if (vendor !== null) {
+            res.json(vendor)
+        }
+    } catch (err) {
+        res.json({"message": "vendor with this id is not registered"})
+    }
 }
--- routes/adminRoute.ts
+++ routes/adminRoute.ts
@@ -1,10 +1,12 @@
 import express, {Request, Response, NextFunction } from "express"
-import { createVendor, getAllVendors, getVendor } from "../controllers";
+import { createVendor, getAllVendors, getVendorById } from "../controllers";
 const router = express.Router()
 
 router.post("/vendor", createVendor)
 router.get("/all_vendors", getAllVendors)
-router.get("/vendor/:id", getVendor)
+router.get("/vendor/:id", getVendorById)
+
+
 
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.json({"message": "Hello its admin"})
