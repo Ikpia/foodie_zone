670f193f0b8043dffb3ac054543637409f87472a
Ikpia <echukwuka97@gmail.com>
2023-10-27 16:58:05 -0700

    modified adminRoute

--- routes/adminRoute.ts
+++ routes/adminRoute.ts
@@ -1,11 +1,11 @@
 import express, {Request, Response, NextFunction } from "express"
-import { createVendor, getAllVendors, getVendorById } from "../controllers";
+import { createVendor, getAllVendors, getVendorById, vendorLogin } from "../controllers";
 const router = express.Router()
 
 router.post("/vendor", createVendor)
 router.get("/all_vendors", getAllVendors)
 router.get("/vendor/:id", getVendorById)
-
+router.post("/login", vendorLogin)
 
 
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
